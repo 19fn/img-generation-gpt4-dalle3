0276a344c1edb4d62029aaf215b1ec9bac799c46
Federico <fcabrera.all@gmail.com>
2024-10-27 12:30:12 -0300

    remove old project

--- playground/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-import os
-
-app = Flask(__name__, template_folder="templates")
-
-app.config["SECRET_KEY"] = os.environ["FLASK_SECRET_KEY"]
-
-from playground import routes
